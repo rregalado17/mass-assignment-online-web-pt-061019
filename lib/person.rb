2065589b14594081f70a6993ea3a86677db307e2
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 16:33:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,20 +2,11 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(attributes)
+  def initialize(name:, birthday:, hair_color:)
   
     @name = name 
     @birthday = birthday
     @hair_color = hair_color
-    @eye_color = eye_color 
-    @height = height
-    @weight = weight
-    @handed = handed
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size
-    @glove_size = glove_size
-    @pant_length = pant_length
-    @pant_width = pant_width
+
   end 
 end
